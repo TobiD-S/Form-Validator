f3545d0f66443ba81812af3f29582f2fb8bdbbce
Tobi <delesafatobi@gmail.com>
2020-04-03 23:30:49 +0100

    Update app.js

--- app.js
+++ app.js
@@ -18,7 +18,11 @@ function showSuccess(input) {
     formControl.className = "form-control success";
 }
 
-
+//Check email is valid
+function isValidEmail(email) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
 
 //Event Listeners
 form.addEventListener('submit', function(e) {
